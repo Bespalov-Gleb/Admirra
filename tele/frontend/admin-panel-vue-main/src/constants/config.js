c465b9290bca0934ccf55ec02b9c2dbc7074d092
Bespalov_Gleb <bengaminplay@gmail.com>
2026-01-24 00:04:12 +0300

    big fix 23.01

--- tele/frontend/admin-panel-vue-main/src/constants/config.js
+++
@@ -1,1 +0,0 @@
-export const DEFAULT_DASHBOARD_PATH = '/projects/create'
--- tele/lead_validator/__init__.py
+++
@@ -1,6 +0,0 @@
-# Lead Validator Module
-# Модуль валидации и фильтрации входящих лидов
-
-from lead_validator.router import router
-
-__all__ = ["router"]
