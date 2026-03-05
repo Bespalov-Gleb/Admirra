d0cbcaa7f42fd36ef1600d4e6176fccd5c151b86
Bespalov_Gleb <bengaminplay@gmail.com>
2026-03-06 01:11:19 +0300

    фронт

--- admin-panel-vue-main/admin-panel-vue-main/src/views/GeneralStats3/components/KeyGoalsStatsV3.vue
+++ admin-panel-vue-main/admin-panel-vue-main/src/views/GeneralStats3/components/KeyGoalsStatsV3.vue
@@ -157,14 +157,14 @@ const updateChart = () => {
       datasets: [
         {
           data,
-          backgroundColor: topGoals.value.map((_, i) => donutColorsDark[i]),
+          backgroundColor: topGoals.value.map((_, i) => donutColors[i]),
           borderWidth: 0,
           hoverOffset: 0,
           weight: 1
         },
         {
           data,
-          backgroundColor: topGoals.value.map((_, i) => donutColors[i]),
+          backgroundColor: topGoals.value.map((_, i) => donutColorsDark[i]),
           borderWidth: 0,
           hoverOffset: 2,
           weight: 2
