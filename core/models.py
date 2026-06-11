7f5075b754f5e8ad69a94589948ea3db1abb6ef2
dess17 <dess17@users.noreply.github.com>
2026-06-11 22:36:52 +0300

    Fix: add Text to SQLAlchemy imports in models.py

--- core/models.py
+++ core/models.py
@@ -1,5 +1,5 @@
 import uuid
-from sqlalchemy import Column, String, ForeignKey, DateTime, Integer, Numeric, Date, Enum, BigInteger, Boolean, UniqueConstraint, JSON, Sequence
+from sqlalchemy import Column, String, Text, ForeignKey, DateTime, Integer, Numeric, Date, Enum, BigInteger, Boolean, UniqueConstraint, JSON, Sequence
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
